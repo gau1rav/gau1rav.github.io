488cb292f5cdad737f1398ed2905c18d29a137dd
gau1rav <34263673+gau1rav@users.noreply.github.com>
2017-12-30 10:42:08 +0530

    Update 1.js

--- js files/1.js
+++ js files/1.js
@@ -2,7 +2,7 @@ function validatefunc() {
                          var x=document.forms["login"]["username"].value;
                          var y=document.forms["login"]["password"].value;
                          if(x=="gaurav"&&y=="gau1rav") {
-                                                        window.open('1.html','_self');
+                                                        window.open("1.html","_self");
                                                                                 }
                          else {
                                window.alert("incorrect username or password");
