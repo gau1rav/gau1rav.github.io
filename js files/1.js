68f1a1c7a027a5b2e73483a96231e43f8bf3b842
gau1rav <34263673+gau1rav@users.noreply.github.com>
2017-12-30 09:07:09 +0530

    Add files via upload

---
+++ js files/1.js
@@ -0,0 +1,10 @@
+function validatefunc() {
+                         var x=document.forms["login"]["username"].value;
+                         var y=document.forms["login"]["password"].value;
+                         if(x=="gaurav"&&y=="gau1rav") {
+                                                        window.open('1.html');
+                                                                                }
+                         else {
+                               window.alert("incorrect username or password");
+                                                                                 }
+                                                                                         }
